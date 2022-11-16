8f80c72021b8a45e23bca5e0fdb5d881f3d95859
Laura Bustamante <91298068+lbte@users.noreply.github.com>
2022-11-16 07:54:59 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,16 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+import re
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
-    return df
+  ruta_gh = "solicitudes_credito.csv"
+  df = pd.read_csv(ruta_gh, sep=";")
+  df.drop(df.columns[0], inplace=True, axis=1)
+  df.iloc[:,0:4] =  df.iloc[:,0:4].apply(lambda x : x.str.lower().str.replace("[_-]", " "), axis=1)
+  df.línea_credito =  df.línea_credito.apply(lambda x : re.sub(r"[_-]", " ", x.lower()))
+  df.monto_del_credito =  df.monto_del_credito.apply(lambda x : re.sub(r"\$\s|\.00|,", "", x)).astype(int)
+  df.fecha_de_beneficio = df.fecha_de_beneficio.apply(lambda x : pd.to_datetime(x, format="%Y/%m/%d") if re.match('^\d{4}\/', x) else pd.to_datetime(x, format="%d/%m/%Y"))
+  df.drop_duplicates(inplace=True)
+  df.dropna(inplace=True)
+  return df
